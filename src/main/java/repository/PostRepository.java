6f0c37ddbf5c300d7fb98a2fc5cc420673382b6e
Andrey <erkcasb@gmail.com>
2021-04-14 15:55:24 +0500

    Initial commit

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,48 @@
+package repository;
+
+import model.Post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+// Stub
+public class PostRepository {
+    private List<Post> postList = new CopyOnWriteArrayList<>();
+    private AtomicInteger postID = new AtomicInteger(0);
+
+    public List<Post> all() {
+        return postList;
+    }
+
+    public Optional<Post> getById(int id) {
+        Post post = null;
+        for (Post p : postList) {
+            if (p.getId() == id) post = p;
+        }
+        return Optional.ofNullable(post);
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            postID.getAndIncrement();
+            post.setId(postID.get());
+        } else {
+            for (Post p : postList) {
+                if (p.getId() == post.getId()) {
+                    p.setContent(post.getContent());
+                } else {
+                    postID.getAndIncrement();
+                    post.setId(postID.get());
+                }
+            }
+        }
+        postList.add(post);
+        return post;
+    }
+
+    public void removeById(int id) {
+        postList.removeIf(p -> p.getId() == id);
+    }
+}
---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,109 @@
+package servlet;
+
+import controller.PostController;
+import repository.PostRepository;
+import service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private static final String PTH = "/netology_servlets_war_exploded/api/posts";
+    private static final String PTH_ID = "/netology_servlets_war_exploded/api/posts/\\d+";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            var path = req.getRequestURI();
+            if (path.equals(PTH)) {
+                controller.all(resp);
+                return;
+            }
+            if (path.matches(PTH_ID)) {
+                var id = Integer.parseInt(path.split("/")[4]);
+                controller.getById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            var path = req.getRequestURI();
+            if (path.equals(PTH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            var path = req.getRequestURI();
+            if (path.matches(PTH_ID)) {
+                var id = Integer.parseInt(path.split("/")[4]);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    /*@Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals("GET") && path.equals("/api/posts")) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }*/
+}
+
